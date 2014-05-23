e561765936f580bd5b1b2c0f7879340e819dd724
JB <jb.blois@iia-laval.fr>
2014-05-23 15:12:20 +0200

    Ajout et remplissage des classes EntretienSPPP.DB

---
+++ EntretienSPPP/EntretienSPPP.DB/OBJECTIF/Objectif.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EntretienSPPP.library
+{
+    public class Objectif
+    {
+        #region Attribut
+        
+        #endregion
+        #region Propriété
+        public int Identifiant { get; set; }
+        public string Mesure { get; set; }
+        public string Description { get; set; }
+        public string Resultat { get; set; }
+        public int IdentifiantEntretien { get; set; }
+        #endregion
+        #region Constructeur
+        public Objectif()
+        {
+                
+        }
+        #endregion
+        #region Méthodes
+        
+        #endregion
+    }
+}
